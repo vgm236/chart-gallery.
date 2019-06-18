e9ed05f70130ff948418e041eac8fa1b6bf02086
vgm236 <50808841+vgm236@users.noreply.github.com>
2019-06-18 00:07:57 -0400

    charts1-2

--- three_charts.py
+++ three_charts.py
@@ -1,5 +1,12 @@
 # three_charts.py
 
+import matplotlib.pyplot as plt # import chart-making package
+import operator # helps to sort correctly
+from pylab import *
+import plotly
+import plotly.graph_objs as go
+
+
 #
 # CHART 1 (PIE)
 #
@@ -12,7 +19,35 @@ pie_data = [
 
 print("----------------")
 print("GENERATING PIE CHART...")
-print(pie_data) # TODO: create a pie chart based on the pie_data
+#print(pie_data) # TODO: create a pie chart based on the pie_data
+
+# FILTERING
+
+companies =  []
+for p in pie_data:
+    companies.append(p["company"])
+
+shares =  []
+for p in pie_data:
+    shares.append(p["market_share"])
+
+# CHART GENERATION (# adapted from: https://plot.ly/python/pie-charts/)
+
+labels = companies
+values = shares
+
+trace = go.Pie(labels=labels, values=values)
+
+plotly.offline.plot([trace], filename="basic_pie_chart.html", auto_open=True)
+
+# OTHER FORM
+
+#fig1, ax1 = plt.subplots()
+#ax1.pie(shares, labels=companies, autopct='%1.1f%%', shadow=True, startangle=90)
+#ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+#plt.show() # need to explicitly "show" the chart window
+
 
 #
 # CHART 2 (LINE)
@@ -31,7 +66,27 @@ line_data = [
 
 print("----------------")
 print("GENERATING LINE GRAPH...")
-print(line_data) # TODO: create a line graph based on the line_data
+#print(line_data) # TODO: create a line graph based on the line_data
+
+# FILTERING
+
+list_dates =  []
+for p in line_data:
+    list_dates.append(p["date"])
+
+stock_price =  []
+for p in line_data:
+    stock_price.append(p["stock_price_usd"])
+
+plotly.offline.plot({
+    "data": [go.Scatter(x=list_dates, y=stock_price)],
+    "layout": go.Layout(title="Stock Price Chart")
+}, auto_open=True)
+
+
+
+#plot(list_dates, stock_price)
+#show()
 
 #
 # CHART 3 (HORIZONTAL BAR)
@@ -49,4 +104,4 @@ bar_data = [
 
 print("----------------")
 print("GENERATING BAR CHART...")
-print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
+#print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
